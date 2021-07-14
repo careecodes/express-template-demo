5397457f1cad50339547d55ea1fee55146e47d60
Caree Youngman <caree@caree.codes>
2021-07-13 20:22:14 -0500

    About and blog page

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 4000;
 
 // Require Express, initalize app
 const express = require('express');
 const app = express();
+
+// Logging Middleware
+const morgan = require('morgan');
+const logger = morgan('tiny');
+app.use(logger);
+
+// Security Middleware
+// const helmet = require('helmet');
+// app.use(helmet());
+
+// Enable static files
+app.use(express.static('public'));
+
+app.all('*', (req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
 
 // Express Template Engine
 const es6Renderer = require('express-es6-template-engine');
@@ -14,7 +31,21 @@ app.set('view engine', 'html');
 
 const server = http.createServer(app);
 
-// Home Page
+// Blog Route Handler
+app.get('/blog', (req, res) => {
+    res.render('blog', {
+        locals: {
+            title: 'Blog',
+        },
+        partials: {
+            head: 'partials/head',
+            nav: 'partials/nav',
+            footer: 'partials/footer',
+        },
+    });
+});
+
+// Home Page Route Handler
 app.get('/', (req, res) => {
     res.render('home', {
         locals: {
@@ -28,13 +59,27 @@ app.get('/', (req, res) => {
     });
 });
 
+// About Page Route Handler
+app.get('/about', (req, res) => {
+    res.render('about', {
+        locals: {
+            title: 'About',
+        },
+        partials: {
+            head: 'partials/head',
+            nav: 'partials/nav',
+            footer: 'partials/footer',
+        },
+    });
+});
+
 app.get('/favicon.ico', (req, res) => {
     res.send('');
 });
 
-app.get('*', (req, res) => {
-    res.send('This is not the home page')
-});
+// app.get('*', (req, res) => {
+//     res.send('This is not the "home" page');
+// });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
